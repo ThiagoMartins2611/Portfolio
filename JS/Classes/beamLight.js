b04ac23f253ec8e8537db2f83fc2e995d8436c81
ThiagoMartins2611 <thiagogames2611@gmail.com>
2024-08-30 16:02:13 -0400

    Add files via upload

---
+++ JS/Classes/beamLight.js
@@ -0,0 +1,18 @@
+export class BeamLight{
+    constructor(){
+        this.velocity;
+        this.color;
+        this.width;
+        this.height;
+    }
+
+    create(height, width, color){
+       let beam = document.createElement('div')
+    
+       beam.id = "beam"
+
+
+       
+    }
+
+}
