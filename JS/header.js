f18d23831dfe58316758dbf84446477314e8d33e
RPGTStudios <174332720+ThiagoMartins2611@users.noreply.github.com>
2025-08-28 20:19:50 -0400

    melhorias no menu moblie e tentativa (falha) de concertar o desaparecimento do menu após clicar na opção

--- JS/header.js
+++ JS/header.js
@@ -1,8 +1,18 @@
 let configSvg = document.querySelector('.configsvg');
 let configMob = document.querySelector('.configMob');
 
+let ulNav = document.querySelector('.navigation');
+let liNav = Array.from(ulNav.children)
 
 
+
+liNav.forEach(element => {
+    const a = Array.from(element.children)[0];
+    a.onclick = () =>{
+        console.log("a")
+        
+    }
+});
 
 
 configSvg.onclick = () => {
@@ -27,10 +37,18 @@ configSvg.onclick = () => {
 	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
 	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
 	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
-</svg>`;
-        
-
-        
+</svg>`;   
         
     }
 }
+
+window.addEventListener('resize',function(){
+
+    const largura = window.innerWidth;
+
+    if(largura > 930){
+        configMob.style.display = 'none'
+        configSvg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"/></svg>`
+    }
+
+});
