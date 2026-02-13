84adc48771e968d5eec6aedaa556af6645289617
Thiago Costa Martins <thiagogames2611@gmail.com>
2026-02-13 14:55:45 -0300

    carinho no rosto

--- JS/mouse.js
+++ JS/mouse.js
@@ -1,5 +1,40 @@
-const card = document.getElementById('cabecaCompleta');
+const head = document.getElementById('cabecaCompleta');
 const container = document.querySelector('.astroHead');
+
+const olhos = document.getElementsByClassName('olho');
+const pupila = document.getElementsByClassName('pupila');
+
+const arrayOlhos = Array.from(olhos);
+const arrayPupilas = Array.from(pupila);
+
+let mouseDown = false;
+
+
+function faceFeliz(){
+
+    if(mouseDown){
+        arrayOlhos.forEach(olho => {
+            olho.className = 'olho feliz';
+        });
+
+        arrayPupilas.forEach(pupila => {
+            pupila.style.display = 'none';
+        });
+    }
+ 
+}
+
+function resetarFace(){
+    arrayOlhos.forEach(olho => {
+        olho.className = 'olho';
+    });
+
+
+    arrayPupilas.forEach(pupila => {
+        pupila.style.display = 'block';
+    });
+}
+
 
 document.addEventListener('mousemove', (e) => {
     
@@ -13,9 +48,31 @@ document.addEventListener('mousemove', (e) => {
 
     const rotateX = mouseY * -25; 
     const rotateY = mouseX * 25;
-    
+
+    arrayPupilas.forEach(pupila => {
+        pupila.style.transform = `translateX(${mouseX * 10}px) translateY(${mouseY * 20}px)`;
+    });
+
+
    
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    head.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
+
+
+head.addEventListener('mousedown', () => {
+
+    mouseDown = true;
+
+    setTimeout(() => {
+        faceFeliz();
+    }, 500); 
+
+});
+
+document.addEventListener('mouseup', () => {
+    mouseDown = false;
+    resetarFace();
+});
+
 
 
